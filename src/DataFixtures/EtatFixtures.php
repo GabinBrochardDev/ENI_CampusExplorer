9c657640e48af1f5f5a17ee86e33fb23001be7f6
GabinBrochardDev <gbrochard.dev@gmail.com>
2024-09-24 14:43:27 +0200

    add fixture Etat

---
+++ src/DataFixtures/EtatFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Etat;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class EtatFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Les valeurs à insérer dans l'entité Etat
+        $etats = [
+            'Ouverte',
+            'Clôturée',
+            'En cours',
+            'Terminée',
+            'Annulée',
+            'Historisée'
+        ];
+
+        foreach ($etats as $libelle) {
+            $etat = new Etat();
+            $etat->setLibelle($libelle);
+            $manager->persist($etat);  // Persist l'entité Etat
+        }
+
+        // Envoie les entités dans la base de données
+        $manager->flush();
+    }
+}
