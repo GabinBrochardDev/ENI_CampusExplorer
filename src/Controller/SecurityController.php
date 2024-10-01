c1e771dd10cfbec95a1be70f7aba4156730811b5
MathisDsc <151618538+MathisDsc@users.noreply.github.com>
2024-10-01 14:00:49 +0200

    login user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Security;
 
 class SecurityController extends AbstractController
 {
+    private $participantRepository;
+
+    public function __construct(ParticipantRepository $participantRepository)
+    {
+        $this->participantRepository = $participantRepository;
+    }
+
     #[Route('/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request, Security $security): Response
     {
         // Si l'utilisateur est déjà connecté, redirigez-le
         if ($this->getUser()) {
@@ -20,11 +30,20 @@ class SecurityController extends AbstractController
         // Récupérer les erreurs de connexion
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // Dernier nom d'utilisateur saisi
-        $nom = $authenticationUtils->getLastUsername();
+        // Dernier identifiant saisi (email ou pseudo)
+        $identifier = $authenticationUtils->getLastUsername();
+
+        // Récupérer l'utilisateur soit par pseudo, soit par email
+        $participant = $this->participantRepository->findOneBy(['email' => $identifier]) 
+            ?? $this->participantRepository->findOneBy(['pseudo' => $identifier]);
+
+        if ($participant) {
+            // Logique d'authentification ici si nécessaire
+            $security->login($participant);
+        }
 
         return $this->render('security/login.html.twig', [
-            'nom' => $nom,
+            'nom' => $identifier,
             'error' => $error,
         ]);
     }
